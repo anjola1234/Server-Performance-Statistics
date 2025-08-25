83b175daae6010835516a80b59c7d09447e9dd2a
anjola1234 <aaniyikaye.2202928@stu.cu.edu.ng>
2025-08-25 15:31:54 +0100

    ssh

---
+++ server-stats.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "===TOTAL CPU USAGE==="
+mpstat 1 1 | awk '/Average:/ {print 100-$NF"%used"}'
+
+echo -e "\n===TOTAL MEMORY USAGE==="
+free --mebi | awk '/Mem:/ {
+    used=$3; total=$2; free=$4; available=$7;
+
+    used_perc = used/total * 100;
+    free_perc = free/total * 100;
+    avail_perc = available/total * 100;
+    
+    used_gib = used/1024;
+    total_gib = total/1024;
+    free_gib = free/1024;
+    avail_gib = available/1024;
+
+    printf " Used: %d Mib(%.2f Gib) / %d Mib(%.2f Gib) (%.2f%%)\n",\
+           used, used_gib, total, total_gib, used_perc;
+
+    printf " Free: %d Mib(%.2f Gib) / %d Mib(%.2f Gib) (%.2f%%)\n",\
+           free, free_gib, total, free_gib, free_perc;
+
+    printf " Available: %d Mib(%.2f Gib) / %d Mib(%.2f Gib) (%.2f%%)\n",\
+           available, avail_gib, total, avail_gib, avail_perc;
+}'	   
+
+
+echo -e "\n===TOTAL DISK USAGE==="
+df -B1M --total | awk '/^total/ {
+    used=$3; total=$2; available=$4;
+
+    used_perc = used/total * 100;
+    avail_perc = available/total * 100;
+
+    sub("M","",used); sub("M","",total); sub("M","",available);
+
+    used_gib = used/1024;
+    total_gib = total/1024;
+    avail_gib = available/1024;
+
+    printf " Used: %d Mib(%.2f Gib) / %d Mib(%.2f Gib) (%.2f%%)\n",\
+           used, used_gib, total, total_gib, used_perc;
+
+    printf " Available: %d Mib(%.2f Gib) / %d Mib(%.2f Gib) (%.2f%%)\n",\
+           available, avail_gib, total, avail_gib, avail_perc;
+
+}'
+
+
+echo -e "\n===TOP 5 CPU PROCESSES==="
+ps -eo pid,ppid,user,cmd,%cpu,%mem --sort=-%cpu | head -n 6
+
+
+echo -e "\n===TOP 5 MEMORY PROCESSES==="
+ps -eo pid,ppid,user,cmd,%cpu,%mem --sort=-%mem | head -n 6
+
+sudo apt install
+sudo apt install build-essential dkms linux-headers-$(uname -r)
